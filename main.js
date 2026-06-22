4d244b22223ca93a6f8574e830da4cad5655c8ce
YodraB <YodraB@users.noreply.github.com>
2026-06-21 21:49:14 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 import * as THREE from 'three';
 
 function main() {
-	const canvas = document.querySelector('#myCanvas');
+	const canvas = document.querySelector('#c');
 	const renderer = new THREE.WebGLRenderer({antialias: true, canvas});
 	
 	const fov = 75;
