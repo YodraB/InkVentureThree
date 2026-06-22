107a394a3ad73ab03588aefced44ba33a298f781
YodraB <YodraB@users.noreply.github.com>
2026-06-21 21:53:32 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,29 +1,20 @@
 import * as THREE from 'three';
 
-function main() {
-	const canvas = document.querySelector('#c');
-	const renderer = new THREE.WebGLRenderer({antialias: true, canvas});
-	
-	const fov = 75;
-	const aspect = 2;  // the canvas default
-	const near = 0.1;
-	const far = 5;
-	const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
-	
-	camera.position.z = 2;
-	
-	const scene = new THREE.Scene();
-	
-	const boxWidth = 1;
-	const boxHeight = 1;
-	const boxDepth = 1;
-	const geometry = new THREE.BoxGeometry(boxWidth, boxHeight, boxDepth);
-	
-	const material = new THREE.MeshBasicMaterial({color: 0x44aa88});
-	
-	const cube = new THREE.Mesh(geometry, material);
-	
-	scene.add(cube);
-	
-	renderer.render(scene, camera);
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize( window.innerWidth, window.innerHeight );
+document.body.appendChild( renderer.domElement );
+
+const geometry = new THREE.BoxGeometry( 1, 1, 1 );
+const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+const cube = new THREE.Mesh( geometry, material );
+scene.add( cube );
+
+camera.position.z = 5;
+
+function animate( time ) {
+  renderer.render( scene, camera );
 }
+renderer.setAnimationLoop( animate );
